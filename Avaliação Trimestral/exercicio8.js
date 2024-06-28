ae508820365e2c0c6c6f21353c9b498c6cb75547
Maarcoos0 <marcos.pauloferreirade.moraes@escola.pr.gov.br>
2024-06-28 11:51:23 -0300

    Conclusão
    Terminada a avaliação

---
+++ Avaliação Trimestral/exercicio8.js
@@ -0,0 +1,14 @@
+const a = 1
+const b = 2
+const c = 3
+const d = 4
+const e = 5
+const f = 6
+
+console.log("os coeficientes a,b,c,d,e e f são respequitivamentes : "+a,+b,+c,+d,+e,+f)
+
+let x = (c*e)-(b*f)/(a*e)-(b*d)
+let y = (a*f)-(c*d)/(a*e)-(b*d)
+
+console.log("X = "+x)
+console.log("Y = "+y)
